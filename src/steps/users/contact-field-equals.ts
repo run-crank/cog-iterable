6dcd64e879c143f02413dc2d04dc7a6c18b6d64a
Angelito Salud <asalud@you-source.com>
2020-01-10 14:12:10 +0800

    changed users to contacts in comments

--- src/steps/users/contact-field-equals.ts
+++ src/steps/users/contact-field-equals.ts
@@ -56,7 +56,7 @@ export class ContactFieldEquals extends BaseStep implements StepInterface {
         // If no results were found, return an error.
         return this.error('No contact found for email %s', [email]);
       } else if (!apiRes.user.hasOwnProperty(field) && !apiRes.user.dataFields.hasOwnProperty(field)) {
-        // If the given field does not exist on the user, return an error.
+        // If the given field does not exist on the contact, return an error.
         return this.error('The %s field does not exist on contact %s', [field, email]);
       } else if (this.compare(operator, apiRes.user.dataFields[field], expectedValue) || this.compare(operator, apiRes.user[field], expectedValue)) {
         // If the value of the field matches expectations, pass.
