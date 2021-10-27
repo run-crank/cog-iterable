573fb0250e6848f3a2a25d93d2316ffe59dca0a3
Russell Hart <russell.hart@stackmoxie.com>
2021-10-27 11:44:18 -0400

    Implement caching for Iterable contacts

---
+++ test/client/caching-client-wrapper.ts
@@ -0,0 +1,146 @@
+import * as chai from 'chai';
+import { default as sinon } from 'ts-sinon';
+import * as sinonChai from 'sinon-chai';
+import 'mocha';
+
+import { CachingClientWrapper } from '../../src/client/caching-client-wrapper';
+
+chai.use(sinonChai);
+
+describe('CachingClientWrapper', () => {
+  const expect = chai.expect;
+  let cachingClientWrapperUnderTest: CachingClientWrapper;
+  let clientWrapperStub: any;
+  let redisClientStub: any;
+  let idMap: any;
+
+  beforeEach(() => {
+    clientWrapperStub = {
+      getContactByEmail: sinon.spy(),
+      createOrUpdateContact: sinon.spy(),
+      deleteContactByEmail: sinon.spy(),
+    };
+
+    redisClientStub = {
+      get: sinon.spy(),
+      setex: sinon.spy(),
+      del: sinon.spy(),
+    };
+
+    idMap = {
+      requestId: '1',
+      scenarioId: '2',
+      requestorId: '3',
+    };
+  });
+
+  it('getContactByEmail using original function', (done) => {
+    const expectedEmail = 'any@example.com';
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.getAsync = sinon.stub().returns(false);
+    cachingClientWrapperUnderTest.getContactByEmail(expectedEmail);
+
+    setTimeout(() => {
+      expect(clientWrapperStub.getContactByEmail).to.have.been.calledWith(expectedEmail);
+      done();
+    });
+  });
+
+  it('getContactByEmail using cache', (done) => {
+    const expectedEmail = 'any@example.com';
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.getAsync = sinon.stub().returns('"expectedCachedValue"');
+    let actualCachedValue: string;
+    (async () => {
+      actualCachedValue = await cachingClientWrapperUnderTest.getContactByEmail(expectedEmail);
+    })();
+
+    setTimeout(() => {
+      expect(clientWrapperStub.getContactByEmail).to.not.have.been.called;
+      expect(actualCachedValue).to.equal('expectedCachedValue');
+      done();
+    });
+  });
+
+  it('deleteContactByEmail', (done) => {
+    const expectedEmail = 'test@example.com';
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.clearCache = sinon.spy();
+    cachingClientWrapperUnderTest.deleteContactByEmail(expectedEmail);
+
+    setTimeout(() => {
+      expect(cachingClientWrapperUnderTest.clearCache).to.have.been.called;
+      expect(clientWrapperStub.deleteContactByEmail).to.have.been.calledWith(expectedEmail);
+      done();
+    });
+  });
+
+  it('createOrUpdateContact', (done) => {
+    const request = {
+      email: 'test@example.com',
+      dataFields: {
+        name: 'bob',
+      },
+      returnRepresentation: true,
+    };
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.clearCache = sinon.spy();
+    cachingClientWrapperUnderTest.createOrUpdateContact(request);
+
+    setTimeout(() => {
+      expect(cachingClientWrapperUnderTest.clearCache).to.have.been.called;
+      expect(clientWrapperStub.createOrUpdateContact).to.have.been.calledWith(request);
+      done();
+    });
+  });
+
+  it('getCache', (done) => {
+    redisClientStub.get = sinon.stub().yields();
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.getCache('expectedKey');
+
+    setTimeout(() => {
+      expect(redisClientStub.get).to.have.been.calledWith('expectedKey');
+      done();
+    });
+  });
+
+  it('setCache', (done) => {
+    redisClientStub.setex = sinon.stub().yields(); 
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.getCache = sinon.stub().returns(null);
+    cachingClientWrapperUnderTest.cachePrefix = 'testPrefix';
+    cachingClientWrapperUnderTest.setCache('expectedKey', 'expectedValue');
+
+    setTimeout(() => {
+      expect(redisClientStub.setex).to.have.been.calledWith('expectedKey', 55, '"expectedValue"');
+      done();
+    });
+  });
+
+  it('delCache', (done) => {
+    redisClientStub.del = sinon.stub().yields();
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.delCache('expectedKey');
+
+    setTimeout(() => {
+      expect(redisClientStub.del).to.have.been.calledWith('expectedKey');
+      done();
+    });
+  });
+
+  it('clearCache', (done) => {
+    redisClientStub.del = sinon.stub().yields();
+    cachingClientWrapperUnderTest = new CachingClientWrapper(clientWrapperStub, redisClientStub, idMap);
+    cachingClientWrapperUnderTest.cachePrefix = 'testPrefix';
+    cachingClientWrapperUnderTest.getCache = sinon.stub().returns(['testKey1', 'testKey2'])
+    cachingClientWrapperUnderTest.clearCache();
+
+    setTimeout(() => {
+      expect(redisClientStub.del).to.have.been.calledWith('testKey1');
+      expect(redisClientStub.del).to.have.been.calledWith('testKey2');
+      expect(redisClientStub.setex).to.have.been.calledWith('cachekeys|testPrefix');
+      done();
+    });
+  });
+});
